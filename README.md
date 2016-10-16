9b3a69f83538bda5108773982e9d84ad4774b500
zuojj <cuew1987@163.com>
2016-10-16 21:30:35 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # weibo_message
 chat with microsoft xiaobing  based on python
+
+## 模拟微博登录
+weibo_login.py
+
+## 与微软小冰私信
+weibo_xiaobing.py
--- weibo_xiaobing.py
+++ weibo_xiaobing.py
@@ -1,5 +1,9 @@
 #coding=utf8
 
+# 2016-10-10
+# @author Benjamin(zuojj.com@gmail.com)
+# @home http://www.zuojj.com
+
 import time
 import json
 import base64
@@ -8,45 +12,16 @@ import sqlite3
 import requests
 from win32.win32crypt import CryptUnprotectData
 
-# @author Benjamin(zuojj.com@gmail.com)
+# @method getPrivateMsgCookie
+# @description 获取私信发送请求头cookie, 本例获取windows7 chrome browser cookie
+# @param host 当前host
+# @return cookie
 
-def login(username, password):
-    username = base64.b64encode(username.encode('utf-8')).decode('utf-8')
+def getPrivateMsgCookie(host = '.weibo.com'):
+    # windows7 Chrome cookies file path
+    cookiepath = os.environ['LOCALAPPDATA'] + r"\Google\Chrome\User Data\Default\Cookies"
 
-    postData = {
-        "entry": "sso",
-        "gateway": "1",
-        "from": "null",
-        "savestate": "30",
-        "useticket": "0",
-        "pagerefer": "",
-        "vsnf": "1",
-        "su": username,
-        "service": "sso",
-        "sp": password,
-        "sr": "1440*900",
-        "encoding": "UTF-8",
-        "cdult": "3",
-        "domain": "sina.com.cn",
-        "prelt": "0",
-        "returntype": "TEXT",
-    }
-    loginURL = r'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)'
-    session = requests.Session()
-    res = session.post(loginURL, data = postData)
-    jsonStr = res.content.decode('gbk')
-    info = json.loads(jsonStr)
-    if info["retcode"] == "0":
-        print('login success')
-    else:
-        print("login failed，the reason： %s" % info["reason"])
-
-#login('cuew1987@163.com', 'xxxx')
-
-def getPrivateMsgCookie(host='.weibo.com'):
-    cookiepath = os.environ['LOCALAPPDATA']+r"\Google\Chrome\User Data\Default\Cookies"
-
-    sql="select host_key,name,encrypted_value from cookies where host_key='%s'" % host
+    sql = "select host_key,name,encrypted_value from cookies where host_key='%s'" % host
     with sqlite3.connect(cookiepath) as conn:
         cu = conn.cursor()
         privateMsgCookie = {
@@ -62,6 +37,11 @@ def getPrivateMsgCookie(host='.weibo.com'):
 
 cookie = getPrivateMsgCookie()
 
+
+# @method postMsg
+# @description 给微软小冰发送私信
+# @param msg 私信内容
+# @return info 响应info
 def postMsg(msg):
     url = "http://weibo.com/aj/message/add?"
     data = "ajwvr=6&__rnd="+ str(int(time.time())) +"&location=msgdialog&module=msgissue&style_id=1&text=" + msg + "&uid=5175429989&tovfids=&fids=&el=[object HTMLDivElement]&_t=0"
@@ -85,26 +65,28 @@ def postMsg(msg):
     info = json.loads(jsonStr)
     return info
 
+# @method getMsg
+# @description 递归获取小冰回答信息，由于获取延迟，采用递归方式
+# @msg 发送给小冰信息
+# @return 返回小冰回答信息
+
+get_url = "http://m.weibo.cn/msg/messages?uid=5175429989&page=1"
+get_headers = {
+    "Host": "m.weibo.cn",
+    "Connection": "keep-alive",
+    "Cache-Control": "max-age=0",
+    "Upgrade-Insecure-Requests": str(1),
+    "X-Requested-With": "XMLHttpRequest",
+    "Referer": "http://m.weibo.cn/msg/chat?uid=5175429989",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8,ko;q=0.6,en;q=0.4,zh-TW;q=0.2,fr;q=0.2",
+    "Cookie": cookie
+}
+
 def getMsg(msg):
-    url = "http://m.weibo.cn/msg/messages?uid=5175429989&page=1"
-    get_headers = {
-        "Host": "m.weibo.cn",
-        "Connection": "keep-alive",
-        "Cache-Control": "max-age=0",
-        "Upgrade-Insecure-Requests": str(1),
-        "X-Requested-With": "XMLHttpRequest",
-        "Referer": "http://m.weibo.cn/msg/chat?uid=5175429989",
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, sdch",
-        "Accept-Language": "zh-CN,zh;q=0.8,ko;q=0.6,en;q=0.4,zh-TW;q=0.2,fr;q=0.2",
-        "Cookie": cookie
-    }
-
-    return repeatGet(url, get_headers, msg)
-
-def repeatGet(url, get_headers, msg):
-    response = requests.get(url, headers=get_headers)
+    response = requests.get(get_url, headers=get_headers)
     result = ""
     if response:
         obj = json.loads(response.text)
@@ -120,6 +102,7 @@ def repeatGet(url, get_headers, msg):
                 result = result + str(obj['data'][0]['attachment'][0]['filename'].encode('utf-8'))
     return result
 
+
 if __name__ == "__main__":
     while True:
         msg = input('say: ')
@@ -133,3 +116,4 @@ if __name__ == "__main__":
             print("send failed")
 
         print("xiaobin: " + result)
+
